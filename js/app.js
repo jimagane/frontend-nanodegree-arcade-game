e0e08fb108399c287b383b7afce821b63fbe4d53
Jane <jimagane@gmail.com>
2018-06-29 14:41:55 -0700

    feat: score and player pick

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 let score = 0;
+let scorelog = document.querySelector('.scorenumber');
+let heartStatus = document.querySelectorAll('.heart');
+let heartlog = document.querySelectorAll('.show');
+let allIcons = document.querySelectorAll('.char');
+let icon ='images/char-boy.png';
+
+
 
 class Enemy {
     constructor(x, y, speed) {
@@ -27,6 +34,7 @@ class Enemy {
       if ((this.x-70 <= player.x && this.x+80>= player.x) && this.y == player.y) {
         player.resetPlayer();
       }
+
     }
 }
 
@@ -44,16 +52,24 @@ var enemy11 = new Enemy(-900, 145, Math.random()*5);
 var enemy12 = new Enemy(-900, 230, Math.random()*5);
 
 class Player {
-    constructor(x=200, y=400) {
+    constructor(x=200, y=400, icon) {
       this.x = x;
       this.y = y;
-      this.icon = 'images/char-boy.png';
+      this.icon = icon;
     }
 
     update(dt) {
       if (this.y < 0) {
         player.resetPlayer();
       }
+      scorelog.innerText = score;
+
+      allIcons.forEach(function(selectedPlayer) {
+      selectedPlayer.addEventListener('click', function(e) {
+        player.icon = this.id;
+        
+      })
+      });
     }
 
     render() {
@@ -79,18 +95,63 @@ class Player {
       setTimeout(function() {
         if (player.y < 0) {
           score += 1;
-          console.log(score);
         }
         player.x = 200;
         player.y = 400;
       }, 50);
     }
+
+    choosePlayer() {
+      allIcons.forEach(function(selectedPlayer) {
+        selectedPlayer.addEventListener('click', function(e) {
+          player.icon=this.id;
+          return this.id;
+        })
+    });
+    }
 }
 
+class SpecialItem {
+    constructor(x, y, speed, sprite, points) {
+      this.x = x;
+      this.y = y;
+      this.speed = speed;
+      this.sprite = sprite;
+      this.points = points;
+    }
+
+    update(dt) {
+      this.x += this.speed;
+      if(this.x > 600) {
+        this.x = -10000;
+      }
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
+      this.checkCollisions();
+    }
+
+    render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    checkCollisions() {
+      if ((this.x-70 <= player.x && this.x+80>= player.x) && this.y == player.y) {
+        setTimeout(this.x = -10000, 100);
+        score += this.points;
+      }
+    }
+}
 var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8, enemy9, enemy10, enemy11, enemy12];
-var player = new Player();
+var player = new Player(200, 400, icon);
 
-
+var blueGem = new SpecialItem(-500, 60, Math.random()*5, 'images/Gem Blue.png', 10);
+var greenGem = new SpecialItem(-1200, 230, Math.random()*5, 'images/Gem Green.png', 20);
+var orangeGem = new SpecialItem(-1800, 145, Math.random()*5, 'images/Gem Orange.png', 30);
+var heart = new SpecialItem(-1000, 60, Math.random()*5, 'images/Heart.png');
+var key = new SpecialItem(-3000, 145, Math.random()*5, 'images/Key.png', 100);
+var star = new SpecialItem(-8000, 60, Math.random()*5, 'images/Star.png', 1000);
+var bonusItems = [blueGem, greenGem, orangeGem, key, star];
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. Provided by Udacity for this project.
 document.addEventListener('keyup', function(e) {
--- js/engine.js
+++ js/engine.js
@@ -93,6 +93,9 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
+        bonusItems.forEach(function(item) {
+            item.update(dt);
+        });
         player.update();
     }
 
@@ -152,7 +155,9 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
-
+        bonusItems.forEach(function(item) {
+            item.render();
+        });
         player.render();
     }
 
@@ -173,7 +178,18 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Gem Blue.png',
+        'images/Gem Green.png',
+        'images/Gem Orange.png',
+        'images/Heart.png',
+        'images/Key.png',
+        'images/Star.png'
+
     ]);
     Resources.onReady(init);
 
