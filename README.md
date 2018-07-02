bc155c04b3c1ef2c291f89664a2c01c371676f59
Jane <jimagane@gmail.com>
2018-07-02 16:02:05 -0700

    final readme and comments

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
-frontend-nanodegree-arcade-game
-===============================
+# Classic Arcade Game: Frogger
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+Creates a game canvas of enemies and special objects player must navigate through to score points. Game objective is to score as many points as possible by collecting special items or crossing successfully from grass to water while avoiding enemy bugs. When all 4 heart life are used, game is over.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## Getting Started
+
+Arcade game project can be cloned from github repository:
+https://github.com/jimagane/memory_game_project.git
+
+### Prerequisites
+
+Use text editor to edit HTML, CSS, and JavaScript resource, engine, and app code and a web browser to view and play project game.
+
+### Installing and Usage
+
+Clone program files from repository to edit. Open index.html file on web browser window to run game. Refresh page to re-randomize object speeds.
+
+The game draws a canvas of grass, stone, and water. It renders enemy, player, and special objects continuously to create movement across page. If player and enemy inhabit same space or come into contact, a heart life is taken and player position is reset. Likewise if a player and special object come into contact, player gains extra points or a heart life. When player successfully reaches water, they get 1 point and player position is reset.
+
+When all heart lives are lost, game over message is displayed with final score. Upon closing message the score, heart life, and player position is reset. If the speeds of enemy and special objects would like to be re-randomized, page must be refreshed.
+
+## Running Tests
+
+Refresh browser page to test modified code.
+
+## Contributing
+
+This repository for memory game project is for Udacity Front-End Web Developer Nanodegree course. Therefore, will not be accepting pull requests at this time.
+
+For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+
+## Dependencies
+
+
+
+## Acknowledgements
+
+Starter HTML and CSS code, JS engine and resource code, and images provided by Udacity for this project was used.
--- js/app.js
+++ js/app.js
@@ -1,75 +1,160 @@
 let score = 0;
 let scorelog = document.querySelector('.scorenumber');
-let heartStatus = document.querySelectorAll('.heart');
 let heartlog = [];
+let heartStatus = document.querySelectorAll('.heart');
+let icon = 'images/char-boy.png';
 let allIcons = document.querySelectorAll('.char');
-let icon ='images/char-boy.png';
-let collision = 0;
-var box1=document.querySelector('#box1');
-var box2=document.querySelector('#box2');
-var box3=document.querySelector('#box3');
-var box4=document.querySelector('#box4');
-var box5=document.querySelector('#box5');
+let box1 = document.querySelector('#box1');
+let box2 = document.querySelector('#box2');
+let box3 = document.querySelector('#box3');
+let box4 = document.querySelector('#box4');
+let box5 = document.querySelector('#box5');
 
-class Enemy {
-    constructor(x, y, speed) {
-      this.x = x;
-      this.y = y;
-      this.speed = speed;
-      this.sprite = 'images/enemy-bug.png';
+// Displays selector image under selected character in header.
+function checkPlayer() {
+    if (player.icon == "images/char-boy.png") {
+        box1.classList.remove('show');
+        box2.classList.add('show');
+        box3.classList.add('show');
+        box4.classList.add('show');
+        box5.classList.add('show');
     }
-
-    update(dt) {
-      this.x += this.speed;
-      if(this.x > 600) {
-        this.x = -1000;
-      }
-      // You should multiply any movement by the dt parameter
-      // which will ensure the game runs at the same speed for
-      // all computers.
-      this.checkCollisions();
+    if (player.icon == "images/char-cat-girl.png") {
+        box1.classList.add('show');
+        box2.classList.remove('show');
+        box3.classList.add('show');
+        box4.classList.add('show');
+        box5.classList.add('show');
     }
-
-    render() {
-      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    if (player.icon == 'images/char-horn-girl.png') {
+        box1.classList.add('show');
+        box2.classList.add('show');
+        box3.classList.remove('show');
+        box4.classList.add('show');
+        box5.classList.add('show');
     }
-
-    checkCollisions() {
-      if ((this.x-70 <= player.x && this.x+80>= player.x) && this.y == player.y) {
-        player.resetPlayer();
-      }
-
+    if (player.icon == 'images/char-pink-girl.png') {
+        box1.classList.add('show');
+        box2.classList.add('show');
+        box3.classList.add('show');
+        box4.classList.remove('show');
+        box5.classList.add('show');
+    }
+    if (player.icon == 'images/char-princess-girl.png') {
+        box1.classList.add('show');
+        box2.classList.add('show');
+        box3.classList.add('show');
+        box4.classList.add('show');
+        box5.classList.remove('show');
     }
 }
 
-var enemy1 = new Enemy(-100, 60, Math.random()*5);
-var enemy2 = new Enemy(-100, 145, Math.random()*5);
-var enemy3 = new Enemy(-100, 230, Math.random()*5);
-var enemy4 = new Enemy(-300, 60, Math.random()*5);
-var enemy5 = new Enemy(-300, 145, Math.random()*5);
-var enemy6 = new Enemy(-300, 230, Math.random()*5);
-var enemy7 = new Enemy(-600, 60, Math.random()*5);
-var enemy8 = new Enemy(-600, 145, Math.random()*5);
-var enemy9 = new Enemy(-600, 230, Math.random()*5);
-var enemy10 = new Enemy(-900, 60, Math.random()*5);
-var enemy11 = new Enemy(-900, 145, Math.random()*5);
-var enemy12 = new Enemy(-900, 230, Math.random()*5);
+// Updates number of hearts in header as well as game over condition
+function heartCheck() {
+    if (heartlog.length == 0) {
+        heartStatus[3].classList.remove('show');
+        heartStatus[2].classList.remove('show');
+        heartStatus[1].classList.remove('show');
+        heartStatus[0].classList.remove('show');
+    }
+    if (heartlog.length == 1) {
+        heartStatus[3].classList.add('show');
+        heartStatus[2].classList.remove('show');
+        heartStatus[1].classList.remove('show');
+        heartStatus[0].classList.remove('show');
+    }
+    if (heartlog.length == 2) {
+        heartStatus[3].classList.add('show');
+        heartStatus[2].classList.add('show');
+        heartStatus[1].classList.remove('show');
+        heartStatus[0].classList.remove('show');
+    }
+    if (heartlog.length == 3) {
+        heartStatus[3].classList.add('show');
+        heartStatus[2].classList.add('show');
+        heartStatus[1].classList.add('show');
+        heartStatus[0].classList.remove('show');
+    }
+    if (heartlog.length == 4) {
+        heartStatus[3].classList.add('show');
+        heartStatus[2].classList.add('show');
+        heartStatus[1].classList.add('show');
+        heartStatus[0].classList.add('show');
+        setTimeout(alert(`
+            Game Over!
+             Score: ${score}
+            Play Again?`), 200);
+        heartStatus[0].classList.remove('show');
+        heartStatus[1].classList.remove('show');
+        heartStatus[2].classList.remove('show');
+        heartStatus[3].classList.remove('show');
+        heartlog = [];
+        score = 0;
+    }
+}
 
-class Player {
-    constructor(x=200, y=400, icon) {
-      this.x = x;
-      this.y = y;
-      this.icon = icon;
+
+class Enemy {
+    constructor(x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = 'images/enemy-bug.png';
     }
 
     update(dt) {
-      if (this.y < 0) {
-        player.resetPlayer();
-      }
-      scorelog.innerText = score;
+        this.x += this.speed;
+        // Loop position once offscreen.
+        if (this.x > 600) {
+            this.x = -1000;
+        }
+        this.checkCollisions();
+    }
 
-      player.choosePlayer();
-      heartCheck();
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    checkCollisions() {
+        if ((this.x-70 <= player.x && this.x+80 >= player.x) && this.y == player.y) {
+            player.resetPlayer();
+        }
+    }
+}
+
+// Vary enemy x and y positions, randomize speed.
+let enemy1 = new Enemy(-100, 60, Math.random()*5);
+let enemy2 = new Enemy(-100, 145, Math.random()*5);
+let enemy3 = new Enemy(-100, 230, Math.random()*5);
+let enemy4 = new Enemy(-300, 60, Math.random()*5);
+let enemy5 = new Enemy(-300, 145, Math.random()*5);
+let enemy6 = new Enemy(-300, 230, Math.random()*5);
+let enemy7 = new Enemy(-600, 60, Math.random()*5);
+let enemy8 = new Enemy(-600, 145, Math.random()*5);
+let enemy9 = new Enemy(-600, 230, Math.random()*5);
+let enemy10 = new Enemy(-900, 60, Math.random()*5);
+let enemy11 = new Enemy(-900, 145, Math.random()*5);
+let enemy12 = new Enemy(-900, 230, Math.random()*5);
+let allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8, enemy9, enemy10, enemy11, enemy12];
+
+class Player {
+    constructor(x = 200, y = 400, icon) {
+        this.x = x;
+        this.y = y;
+        this.icon = icon;
+    }
+
+    update(dt) {
+        // Winning condition
+        if (this.y < 0) {
+            player.resetPlayer();
+        }
+        // Update score in header
+        scorelog.innerText = score;
+        // Update player selection in header
+        player.choosePlayer();
+        // Update heart status in header
+        heartCheck();
     }
 
     render() {
@@ -77,92 +162,88 @@ class Player {
     }
 
     handleInput(action) {
-      if (action == 'up' && this.y > 0) {
-        this.y -= 85;
-      }
-      if (action == 'down' && this.y < 400) {
-        this.y += 85;
-      }
-      if (action == 'left' && this.x > 0) {
-        this.x -= 100;
-      }
-      if (action == 'right' && this.x < 400) {
-        this.x += 100;
-      }
+        if (action == 'up' && this.y > 0) {
+            this.y -= 85;
+        }
+        if (action == 'down' && this.y < 400) {
+            this.y += 85;
+        }
+        if (action == 'left' && this.x > 0) {
+            this.x -= 100;
+        }
+        if (action == 'right' && this.x < 400) {
+            this.x += 100;
+        }
     }
 
     resetPlayer() {
-      setTimeout(function() {
-        if (player.y < 0) {
-          score += 1;
-        }
-        if (player.y > 0 && player.y < 315) {
-          heartlog.push(1);
-        }
-        player.x = 200;
-        player.y = 400;
-      }, 50);
+        setTimeout(function() {
+            // Increase score if player has reached water.
+            if (player.y < 0) {
+              score += 1;
+            }
+            // Decrease heart if player contacts enemy.
+            if (player.y > 0 && player.y < 315) {
+              heartlog.push(1);
+            }
+            player.x = 200;
+            player.y = 400;
+        }, 50);
     }
 
     choosePlayer() {
-      allIcons.forEach(function(selectedPlayer) {
-        selectedPlayer.addEventListener('click', function(e) {
-          player.icon=this.id;
-          checkPlayer();
-
-        })
-    });
+        allIcons.forEach(function(selectedPlayer) {
+            selectedPlayer.addEventListener('click', function(e) {
+              player.icon = this.id;
+              checkPlayer();
+            });
+        });
     }
 }
+
+let player = new Player(200, 400, icon);
 
 class SpecialItem {
     constructor(x, y, speed, sprite, points) {
-      this.x = x;
-      this.y = y;
-      this.speed = speed;
-      this.sprite = sprite;
-      this.points = points;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.sprite = sprite;
+        this.points = points;
     }
 
     update(dt) {
-      this.x += this.speed;
-      if(this.x > 600) {
-        this.x = -10000;
-      }
-      // You should multiply any movement by the dt parameter
-      // which will ensure the game runs at the same speed for
-      // all computers.
-      this.checkCollisions();
-
-
+        this.x += this.speed;
+        if(this.x > 600) {
+            this.x = -10000;
+        }
+        this.checkCollisions();
     }
 
     render() {
-      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
     checkCollisions() {
-      if ((this.x-70 <= player.x && this.x+80>= player.x) && this.y == player.y) {
-        setTimeout(this.x = -10000, 100);
-        score += this.points;
-        if (this.points == 0) {
-          heartlog.pop();
+        if ((this.x-70 <= player.x && this.x+80 >= player.x) && this.y == player.y) {
+            setTimeout(this.x = -10000, 100);
+            score += this.points;
+            // Add heart instead of points for heart item
+            if (this.points == 0) {
+                heartlog.pop();
+            }
         }
-      }
-
     }
 }
-var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8, enemy9, enemy10, enemy11, enemy12];
-var player = new Player(200, 400, icon);
 
-var blueGem = new SpecialItem(-500, 60, Math.random()*5, 'images/Gem Blue.png', 10);
-var greenGem = new SpecialItem(-1200, 230, Math.random()*5, 'images/Gem Green.png', 20);
-var orangeGem = new SpecialItem(-1800, 145, Math.random()*5, 'images/Gem Orange.png', 30);
-var key = new SpecialItem(-3000, 145, Math.random()*5, 'images/Key.png', 100);
-var star = new SpecialItem(-8000, 60, Math.random()*5, 'images/Star.png', 1000);
-var heart = new SpecialItem(-100, 230, Math.random()*5, 'images/Heart.png', 0);
+let blueGem = new SpecialItem(-500, 60, Math.random()*5, 'images/Gem Blue.png', 10);
+let greenGem = new SpecialItem(-1200, 230, Math.random()*5, 'images/Gem Green.png', 20);
+let orangeGem = new SpecialItem(-1800, 145, Math.random()*5, 'images/Gem Orange.png', 30);
+let key = new SpecialItem(-3000, 145, Math.random()*5, 'images/Key.png', 100);
+let star = new SpecialItem(-8000, 60, Math.random()*5, 'images/Star.png', 1000);
+let heart = new SpecialItem(-10000, 230, Math.random()*5, 'images/Heart.png', 0);
+let bonusItems = [blueGem, greenGem, orangeGem, key, star, heart];
 
-var bonusItems = [blueGem, greenGem, orangeGem, key, star, heart];
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. Provided by Udacity for this project.
 document.addEventListener('keyup', function(e) {
@@ -175,84 +256,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-function checkPlayer(){
-if(player.icon=="images/char-boy.png"){
-  box1.classList.remove('show');
-  box2.classList.add('show');
-  box3.classList.add('show');
-  box4.classList.add('show');
-  box5.classList.add('show');
-}
-if(player.icon=="images/char-cat-girl.png"){
-  box1.classList.add('show');
-  box2.classList.remove('show');
-  box3.classList.add('show');
-  box4.classList.add('show');
-  box5.classList.add('show');
-}if(player.icon=='images/char-horn-girl.png'){
-  box1.classList.add('show');
-  box2.classList.add('show');
-  box3.classList.remove('show');
-  box4.classList.add('show');
-  box5.classList.add('show');
-}if(player.icon=='images/char-pink-girl.png'){
-  box1.classList.add('show');
-  box2.classList.add('show');
-  box3.classList.add('show');
-  box4.classList.remove('show');
-  box5.classList.add('show');
-}if(player.icon=='images/char-princess-girl.png'){
-  box1.classList.add('show');
-  box2.classList.add('show');
-  box3.classList.add('show');
-  box4.classList.add('show');
-  box5.classList.remove('show');
-}}
-
-function heartCheck() {
-  if (heartlog.length == 0) {
-    heartStatus[3].classList.remove('show');
-    heartStatus[2].classList.remove('show');
-    heartStatus[1].classList.remove('show');
-    heartStatus[0].classList.remove('show');
-    // return heartlog;
-  }
-  if (heartlog.length == 1) {
-    heartStatus[3].classList.add('show');
-    heartStatus[2].classList.remove('show');
-    heartStatus[1].classList.remove('show');
-    heartStatus[0].classList.remove('show');
-    // return heartlog;
-  }
-  if (heartlog.length == 2) {
-    heartStatus[3].classList.add('show');
-    heartStatus[2].classList.add('show');
-    heartStatus[1].classList.remove('show');
-    heartStatus[0].classList.remove('show');
-    // return heartlog;
-  }
-  if (heartlog.length == 3) {
-    heartStatus[3].classList.add('show');
-    heartStatus[2].classList.add('show');
-    heartStatus[1].classList.add('show');
-    heartStatus[0].classList.remove('show');
-    // return heartlog;
-  }
-  if (heartlog.length == 4) {
-    heartStatus[3].classList.add('show');
-    heartStatus[2].classList.add('show');
-    heartStatus[1].classList.add('show');
-    heartStatus[0].classList.add('show');
-    setTimeout(alert(`Game Over!
- Score: ${score}
-
-Play Again?`), 200);
-    heartStatus[0].classList.remove('show');
-    heartStatus[1].classList.remove('show');
-    heartStatus[2].classList.remove('show');
-    heartStatus[3].classList.remove('show');
-    heartlog=[];
-    score=0;
-  }
-
-}
