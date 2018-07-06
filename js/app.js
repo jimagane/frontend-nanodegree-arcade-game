205ff0a180e31860d03be585d304a08b8ba0d7f1
Jane <jimagane@gmail.com>
2018-07-05 21:45:09 -0700

    fix: errors

--- js/app.js
+++ js/app.js
@@ -10,90 +10,6 @@ let box3 = document.querySelector('#box3');
 let box4 = document.querySelector('#box4');
 let box5 = document.querySelector('#box5');
 
-// Displays selector image under selected character in header.
-function checkPlayer() {
-    if (player.icon == "images/char-boy.png") {
-        box1.classList.remove('show');
-        box2.classList.add('show');
-        box3.classList.add('show');
-        box4.classList.add('show');
-        box5.classList.add('show');
-    }
-    if (player.icon == "images/char-cat-girl.png") {
-        box1.classList.add('show');
-        box2.classList.remove('show');
-        box3.classList.add('show');
-        box4.classList.add('show');
-        box5.classList.add('show');
-    }
-    if (player.icon == 'images/char-horn-girl.png') {
-        box1.classList.add('show');
-        box2.classList.add('show');
-        box3.classList.remove('show');
-        box4.classList.add('show');
-        box5.classList.add('show');
-    }
-    if (player.icon == 'images/char-pink-girl.png') {
-        box1.classList.add('show');
-        box2.classList.add('show');
-        box3.classList.add('show');
-        box4.classList.remove('show');
-        box5.classList.add('show');
-    }
-    if (player.icon == 'images/char-princess-girl.png') {
-        box1.classList.add('show');
-        box2.classList.add('show');
-        box3.classList.add('show');
-        box4.classList.add('show');
-        box5.classList.remove('show');
-    }
-}
-
-// Updates number of hearts in header as well as game over condition
-function heartCheck() {
-    if (heartlog.length == 0) {
-        heartStatus[3].classList.remove('show');
-        heartStatus[2].classList.remove('show');
-        heartStatus[1].classList.remove('show');
-        heartStatus[0].classList.remove('show');
-    }
-    if (heartlog.length == 1) {
-        heartStatus[3].classList.add('show');
-        heartStatus[2].classList.remove('show');
-        heartStatus[1].classList.remove('show');
-        heartStatus[0].classList.remove('show');
-    }
-    if (heartlog.length == 2) {
-        heartStatus[3].classList.add('show');
-        heartStatus[2].classList.add('show');
-        heartStatus[1].classList.remove('show');
-        heartStatus[0].classList.remove('show');
-    }
-    if (heartlog.length == 3) {
-        heartStatus[3].classList.add('show');
-        heartStatus[2].classList.add('show');
-        heartStatus[1].classList.add('show');
-        heartStatus[0].classList.remove('show');
-    }
-    if (heartlog.length == 4) {
-        heartStatus[3].classList.add('show');
-        heartStatus[2].classList.add('show');
-        heartStatus[1].classList.add('show');
-        heartStatus[0].classList.add('show');
-        setTimeout(alert(`
-            Game Over!
-             Score: ${score}
-            Play Again?`), 200);
-        heartStatus[0].classList.remove('show');
-        heartStatus[1].classList.remove('show');
-        heartStatus[2].classList.remove('show');
-        heartStatus[3].classList.remove('show');
-        heartlog = [];
-        score = 0;
-    }
-}
-
-
 class Enemy {
     constructor(x, y, speed) {
         this.x = x;
@@ -103,7 +19,7 @@ class Enemy {
     }
 
     update(dt) {
-        this.x += this.speed;
+        this.x += this.speed*dt*45;
         // Loop position once offscreen.
         if (this.x > 600) {
             this.x = -1000;
@@ -147,14 +63,14 @@ class Player {
     update(dt) {
         // Winning condition
         if (this.y < 0) {
-            player.resetPlayer();
+            this.resetPlayer();
         }
         // Update score in header
         scorelog.innerText = score;
         // Update player selection in header
-        player.choosePlayer();
+        this.choosePlayer();
         // Update heart status in header
-        heartCheck();
+        this.heartCheck();
     }
 
     render() {
@@ -177,27 +93,112 @@ class Player {
     }
 
     resetPlayer() {
+        const self = this;
         setTimeout(function() {
             // Increase score if player has reached water.
-            if (player.y < 0) {
+            if (self.y < 0) {
               score += 1;
             }
             // Decrease heart if player contacts enemy.
-            if (player.y > 0 && player.y < 315) {
+            if (self.y > 0 && self.y < 315) {
               heartlog.push(1);
             }
-            player.x = 200;
-            player.y = 400;
+            self.x = 200;
+            self.y = 400;
         }, 50);
     }
 
     choosePlayer() {
+        const self = this;
         allIcons.forEach(function(selectedPlayer) {
             selectedPlayer.addEventListener('click', function(e) {
-              player.icon = this.id;
-              checkPlayer();
+              self.icon = this.id;
+              self.checkPlayer();
             });
         });
+    }
+
+    // Updates number of hearts in header as well as game over condition
+    heartCheck() {
+        if (heartlog.length == 0) {
+            heartStatus[3].classList.remove('show');
+            heartStatus[2].classList.remove('show');
+            heartStatus[1].classList.remove('show');
+            heartStatus[0].classList.remove('show');
+        }
+        if (heartlog.length == 1) {
+            heartStatus[3].classList.add('show');
+            heartStatus[2].classList.remove('show');
+            heartStatus[1].classList.remove('show');
+            heartStatus[0].classList.remove('show');
+        }
+        if (heartlog.length == 2) {
+            heartStatus[3].classList.add('show');
+            heartStatus[2].classList.add('show');
+            heartStatus[1].classList.remove('show');
+            heartStatus[0].classList.remove('show');
+        }
+        if (heartlog.length == 3) {
+            heartStatus[3].classList.add('show');
+            heartStatus[2].classList.add('show');
+            heartStatus[1].classList.add('show');
+            heartStatus[0].classList.remove('show');
+        }
+        if (heartlog.length == 4) {
+            heartStatus[3].classList.add('show');
+            heartStatus[2].classList.add('show');
+            heartStatus[1].classList.add('show');
+            heartStatus[0].classList.add('show');
+            setTimeout(alert(`
+                Game Over!
+                Score: ${score}
+                Play Again?`), 200);
+            heartStatus[0].classList.remove('show');
+            heartStatus[1].classList.remove('show');
+            heartStatus[2].classList.remove('show');
+            heartStatus[3].classList.remove('show');
+            heartlog = [];
+            score = 0;
+        }
+    }
+
+    // Displays selector image under selected character in header.
+    checkPlayer() {
+        if (player.icon == "images/char-boy.png") {
+            box1.classList.remove('show');
+            box2.classList.add('show');
+            box3.classList.add('show');
+            box4.classList.add('show');
+            box5.classList.add('show');
+        }
+        if (player.icon == "images/char-cat-girl.png") {
+            box1.classList.add('show');
+            box2.classList.remove('show');
+            box3.classList.add('show');
+            box4.classList.add('show');
+            box5.classList.add('show');
+        }
+        if (player.icon == 'images/char-horn-girl.png') {
+            box1.classList.add('show');
+            box2.classList.add('show');
+            box3.classList.remove('show');
+            box4.classList.add('show');
+            box5.classList.add('show');
+        }
+        if (player.icon == 'images/char-pink-girl.png') {
+            box1.classList.add('show');
+            box2.classList.add('show');
+            box3.classList.add('show');
+            box4.classList.remove('show');
+            box5.classList.add('show');
+        }
+        if (player.icon == 'images/char-princess-girl.png') {
+            box1.classList.add('show');
+            box2.classList.add('show');
+            box3.classList.add('show');
+            box4.classList.add('show');
+            box5.classList.remove('show');
+        }
     }
 }
 
@@ -213,7 +214,7 @@ class SpecialItem {
     }
 
     update(dt) {
-        this.x += this.speed;
+        this.x += this.speed*dt*45;
         if(this.x > 600) {
             this.x = -10000;
         }
@@ -237,12 +238,24 @@ class SpecialItem {
 }
 
 let blueGem = new SpecialItem(-500, 60, Math.random()*5, 'images/Gem Blue.png', 10);
+let blueGem2 = new SpecialItem(-20000, 145, Math.random()*5, 'images/Gem Blue.png', 10);
+let blueGem3 = new SpecialItem(-20000, 230, Math.random()*5, 'images/Gem Blue.png', 10);
 let greenGem = new SpecialItem(-1200, 230, Math.random()*5, 'images/Gem Green.png', 20);
+let greenGem2 = new SpecialItem(-20000, 145, Math.random()*5, 'images/Gem Green.png', 20);
+let greenGem3 = new SpecialItem(-20000, 60, Math.random()*5, 'images/Gem Green.png', 20);
 let orangeGem = new SpecialItem(-1800, 145, Math.random()*5, 'images/Gem Orange.png', 30);
+let orangeGem2 = new SpecialItem(-20000, 60, Math.random()*5, 'images/Gem Orange.png', 30);
+let orangeGem3 = new SpecialItem(-20000, 230, Math.random()*5, 'images/Gem Orange.png', 30);
 let key = new SpecialItem(-3000, 145, Math.random()*5, 'images/Key.png', 100);
+let key2 = new SpecialItem(-20000, 230, Math.random()*5, 'images/Key.png', 100);
+let key3 = new SpecialItem(-20000, 60, Math.random()*5, 'images/Key.png', 100);
 let star = new SpecialItem(-8000, 60, Math.random()*5, 'images/Star.png', 1000);
-let heart = new SpecialItem(-10000, 230, Math.random()*5, 'images/Heart.png', 0);
-let bonusItems = [blueGem, greenGem, orangeGem, key, star, heart];
+let star2 = new SpecialItem(-20000, 145, Math.random()*5, 'images/Star.png', 1000);
+let star3 = new SpecialItem(-20000, 230, Math.random()*5, 'images/Star.png', 1000);
+let heart = new SpecialItem(-8000, 230, Math.random()*5, 'images/Heart.png', 0);
+let heart2 = new SpecialItem(-20000, 145, Math.random()*5, 'images/Heart.png', 0);
+let heart3 = new SpecialItem(-20000, 60, Math.random()*5, 'images/Heart.png', 0);
+let bonusItems = [blueGem, blueGem2, blueGem3, greenGem, greenGem2, greenGem3, orangeGem, orangeGem2, orangeGem3, key, key2, key3, star, star2, star3, heart, heart2, heart3];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. Provided by Udacity for this project.
